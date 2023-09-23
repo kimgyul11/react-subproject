6d35352639449c0196c80d3c17672b2cfc5a9334
kimgyul11 <coloo88@naver.com>
2023-09-23 22:22:42 +0900

    초기설정 완료

---
+++ src/utils/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBW5ZcgOf8_tCOpULk5QV_Tw3rMxEwjp_I",
+  authDomain: "react-subproject.firebaseapp.com",
+  projectId: "react-subproject",
+  storageBucket: "react-subproject.appspot.com",
+  messagingSenderId: "513435351485",
+  appId: "1:513435351485:web:58ad3d3cd341a388483ad4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
